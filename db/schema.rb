eb786a607243bffef8c89cfaf043d5da83b3ad12
IDEAGENCY <glongchamp8@gmail.com>
2022-10-27 14:39:54 +0200

    Create MVC Company

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_27_111536) do
+ActiveRecord::Schema.define(version: 2022_10_27_121447) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "zip"
+    t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
