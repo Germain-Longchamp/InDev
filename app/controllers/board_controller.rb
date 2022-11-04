5e387ed5da1c4af1fad578ae699014dec95a331d
IDEAGENCY <glongchamp8@gmail.com>
2022-11-04 09:00:22 +0100

    Designs Integration Global

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -3,5 +3,6 @@ class BoardController < ApplicationController
 
     def index 
         @clients_company = Client.where(:company_id => current_user.company_id).all
+        @designs_company = Design.where(:company_id => current_user.company_id).all
     end
 end
